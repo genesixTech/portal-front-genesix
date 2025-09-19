688379b78ef465c159ea5235638194d213bc2ead
Carloshpjacinto <carlo.henrique37@gmail.com>
2025-09-18 21:47:09 -0300

    feature/create-promotion-banner-and-navigation-menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import NavigationMenu from "@/components/NavigationMenu";
+import PromotionBanner from "@/components/PromotionBanner";
 
 export default function Home() {
   return (
     <>
+      <PromotionBanner />
+
+      <NavigationMenu />
+
+      <p>E ele disse: “que hajam produtos! E o produto foi criado...” (GenesiX 1:3-5)</p>
     </>
   );
 }
---
+++ src/components/PromotionBanner/index.tsx
@@ -0,0 +1,14 @@
+import "./index.scss";
+
+const PromotionBanner = () => {
+  return (
+    <>
+      <div className="promotionBanner">
+        <p>✨ Garanta 25% OFF* em qualquer plano. Cupom:</p>
+        <div className="cupom">LANÇAMENTOX</div>
+      </div>
+    </>
+  );
+};
+
+export default PromotionBanner;
